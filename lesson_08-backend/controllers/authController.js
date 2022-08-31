26b97ea064ccc105c16b66b0bbdfe7cdfaa5efcf
gitdagray <DaveOnEleven@gmail.com>
2022-08-31 16:29:10 -0500

    extended tokens

--- lesson_08-backend/controllers/authController.js
+++ lesson_08-backend/controllers/authController.js
@@ -31,13 +31,13 @@ const login = asyncHandler(async (req, res) => {
             }
         },
         process.env.ACCESS_TOKEN_SECRET,
-        { expiresIn: '1m' }
+        { expiresIn: '15m' }
     )
 
     const refreshToken = jwt.sign(
         { "username": foundUser.username },
         process.env.REFRESH_TOKEN_SECRET,
-        { expiresIn: '1d' }
+        { expiresIn: '7d' }
     )
 
     // Create secure cookie with refresh token 
@@ -80,7 +80,7 @@ const refresh = (req, res) => {
                     }
                 },
                 process.env.ACCESS_TOKEN_SECRET,
-                { expiresIn: '1m' }
+                { expiresIn: '15m' }
             )
 
             res.json({ accessToken })
