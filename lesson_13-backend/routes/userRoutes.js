f214ddfd69eec44f81441bf0f9acacdf555aa0d9
adelpro <47066151+adelpro@users.noreply.github.com>
2022-09-26 15:51:06 +0100

    Update userRoutes.js
    The user creation should not pass by the json web token verification 
    thinks

--- lesson_13-backend/routes/userRoutes.js
+++ lesson_13-backend/routes/userRoutes.js
@@ -3,11 +3,10 @@ const router = express.Router()
 const usersController = require('../controllers/usersController')
 const verifyJWT = require('../middleware/verifyJWT')
 
+router.route('/').post(usersController.createNewUser)
 router.use(verifyJWT)
-
 router.route('/')
     .get(usersController.getAllUsers)
-    .post(usersController.createNewUser)
     .patch(usersController.updateUser)
     .delete(usersController.deleteUser)
 
