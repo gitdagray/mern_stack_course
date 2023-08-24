3576e1219f06b1351ed57693da9e2c0db55df3d6
Zhao Ming Soh <zhaomingsoh@Zhaos-MacBook-Pro.local>
2023-08-24 16:22:01 +0800

    Changed the naming of lesson_03&04 to lesson_03_MongoDB & 04_Contollers

---
+++ lesson_03_MongoDB/models/Note.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const AutoIncrement = require("mongoose-sequence")(mongoose);
+
+const noteSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// create a ticket field inside of the noteSchema that will get the sequential number starting from 500
+noteSchema.plugin(AutoIncrement, {
+  inc_field: "ticket",
+  id: "ticketNums",
+  start_seq: 500,
+});
+
+module.exports = mongoose.model("Note", noteSchema);
---
+++ lesson_04_Controllers/controllers/usersController.js
@@ -0,0 +1,150 @@
+const User = require("../models/User");
+const Note = require("../models/Note");
+const asyncHandler = require("express-async-handler"); // prevent us from using a lot of async try/catch blocks
+const bcrypt = require("bcrypt");
+
+// @desc Get all users
+// @route GET /users
+// @access Private
+const getAllUsers = asyncHandler(async (req, res) => {
+  // Get all users from MongoDB
+  const users = await User.find().select("-password").lean(); // return user as a plain JSON as opposed to getting a full Mongoose document with a bunch of methods such as save() ...
+
+  // If no users
+  if (!users?.length) {
+    return res.status(400).json({ message: "No users found" });
+  }
+
+  res.json(users);
+});
+
+// @desc Create new user
+// @route POST /users
+// @access Private
+const createNewUser = asyncHandler(async (req, res) => {
+  const { username, password, roles } = req.body;
+
+  // Confirm data
+  if (!username || !password || !Array.isArray(roles) || !roles.length) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  // Check for duplicate username
+  const duplicate = await User.findOne({ username }).lean().exec(); // Mongoose queries are not real Promises, instead they return thenable objects, but if you do need real Promises, then you should use exec() on the queries.
+
+  if (duplicate) {
+    return res.status(409).json({ message: "Duplicate username" });
+  }
+
+  // Hash password
+  const hashedPwd = await bcrypt.hash(password, 10); // salt rounds
+
+  const userObject = { username, password: hashedPwd, roles };
+
+  // Create and store new user
+  const user = await User.create(userObject);
+
+  if (user) {
+    //created
+    res.status(201).json({ message: `New user ${username} created` });
+  } else {
+    res.status(400).json({ message: "Invalid user data received" });
+  }
+});
+
+// @desc Update a user
+// @route PATCH /users
+// @access Private
+const updateUser = asyncHandler(async (req, res) => {
+  const { id, username, roles, active, password } = req.body;
+
+  // Confirm data
+  if (
+    !id ||
+    !username ||
+    !Array.isArray(roles) ||
+    !roles.length ||
+    typeof active !== "boolean"
+  ) {
+    return res
+      .status(400)
+      .json({ message: "All fields except password are required" });
+  }
+
+  // Does the user exist to update?
+  /*
+    .exec() vs no exec() :
+        - exec() is used to execute the query and return a promise that resolves to the result of the query
+            - control over when and how the query is executed.
+        - await or .then() directly returns a promise that resolves to the object it found
+  */
+  const user = await User.findById(id).exec();
+
+  if (!user) {
+    return res.status(400).json({ message: "User not found" });
+  }
+
+  // Check for duplicate
+  /* 
+    If we change the username of the user that we want to update to a username that already exist
+    - duplicate will exist
+    - duplicate will yield a different user_id than the one we're trying to update
+  */
+  const duplicate = await User.findOne({ username }).lean().exec();
+
+  // Allow updates to the original user
+  if (duplicate && duplicate?._id.toString() !== id) {
+    return res.status(409).json({ message: "Duplicate username" });
+  }
+
+  user.username = username;
+  user.roles = roles;
+  user.active = active;
+
+  if (password) {
+    // Hash password
+    user.password = await bcrypt.hash(password, 10); // salt rounds
+  }
+
+  const updatedUser = await user.save();
+
+  res.json({ message: `${updatedUser.username} updated` });
+});
+
+// @desc Delete a user
+// @route DELETE /users
+// @access Private
+const deleteUser = asyncHandler(async (req, res) => {
+  const { id } = req.body;
+
+  // Confirm data
+  if (!id) {
+    return res.status(400).json({ message: "User ID Required" });
+  }
+
+  // Does the user still have assigned notes?
+  const note = await Note.findOne({ user: id }).lean().exec();
+  if (note) {
+    return res.status(400).json({ message: "User has assigned notes" });
+  }
+
+  // Does the user exist to delete?
+  const user = await User.findById(id).exec();
+
+  if (!user) {
+    return res.status(400).json({ message: "User not found" });
+  }
+
+  const result = await user.deleteOne();
+
+  const reply = `Username ${result.username} with ID ${result._id} deleted`;
+
+  res.json(reply);
+});
+
+module.exports = {
+  getAllUsers,
+  createNewUser,
+  updateUser,
+  deleteUser,
+};
