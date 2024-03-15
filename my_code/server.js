31c2a8c6a91054e4cb3240de5922d3d5fec9c0be
Walt-20 <walt.c0des20@gmail.com>
2024-03-14 19:29:04 -0600

    adding custom middleware

--- my_code/server.js
+++ my_code/server.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const { logger } = require('./middleware/logger')
+const { errorHandler } = require('./middleware/errorHandler')
 const PORT = process.env.PORT || 3500
 
+app.use(logger) // custom middleware
 
-app.use('/', express.static(path.join(__dirname, '/public')))
+app.use(express.json()) // built in middleware
+
+// built in middleware
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.use('/', require('./routes/root'))
 
@@ -18,5 +24,7 @@ app.all('*', (req, res) => {
         res.type('txt').send('404 Not Found')
     }
 })
+
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
